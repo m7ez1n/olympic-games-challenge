e0036f5d383a6741c642e132e716b3f3003bc6b1
m7Aei <mateuspm2015@hotmail.com>
2019-08-01 03:38:43 -0300

    Iniciando o controller da competição

---
+++ src/app/controllers/CompetitionController.js
@@ -0,0 +1,21 @@
+const Competition = require('../models/Competition')
+
+class CompetitionController {
+  // CRUD da competição
+  // Listagem
+  async index (req, res) {}
+
+  // Listagem de um jogador só
+  async show (req, res) {}
+
+  // Criação de um novo jogador
+  async store (req, res) {}
+
+  // Update em um jogador
+  async update (req, res) {}
+
+  // Deletar algum jogador
+  async destroy (req, res) {}
+}
+
+module.exports = new CompetitionController()

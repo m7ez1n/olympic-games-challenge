e111d26c6942a3d2a5c40536a09daccb7b32062d
m7Aei <mateuspm2015@hotmail.com>
2019-08-02 00:11:32 -0300

    Fazendo a parte de criação e update da competição

--- src/app/controllers/CompetitionController.js
+++ src/app/controllers/CompetitionController.js
@@ -18,16 +18,30 @@ class CompetitionController {
 
   // Listagem de um jogador só
   async show (req, res) {
-    const competition = Competition.findById(req.params.id)
+    const competition = await Competition.findById(req.params.id)
 
     return res.json(competition)
   }
 
   // Criação de um novo jogador
-  async store (req, res) {}
+  async store (req, res) {
+    const competition = await Competition.create(req.body)
+
+    return res.json(competition)
+  }
 
   // Update em um jogador
-  async update (req, res) {}
+  async update (req, res) {
+    const competition = await Competition.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true
+      }
+    )
+
+    return res.json(competition)
+  }
 
   // Deletar algum jogador
   async destroy (req, res) {}

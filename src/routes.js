30d460fb8af08b3ea3287b0920ef1eb50516ec87
m7Aei <mateuspm2015@hotmail.com>
2019-08-02 00:38:48 -0300

    Fazendo as rotas do CRUD da aplicação

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,12 @@ const express = require('express')
 
 const routes = express.Router()
 
+const CompetitionController = require('./app/controllers/CompetitionController')
+
+routes.get('/competitions', CompetitionController.index)
+routes.get('/competitions/:id', CompetitionController.show)
+routes.post('/competitions', CompetitionController.store)
+routes.put('/competitions/:id', CompetitionController.update)
+routes.delete('/competitions/:id', CompetitionController.destroy)
+
 module.exports = routes

d8d7276e29b5020c3750939ace983bdd8d5c629b
m7Aei <mateuspm2015@hotmail.com>
2019-08-03 17:59:00 -0300

    Terminando de criar rotas dos players

--- src/routes.js
+++ src/routes.js
@@ -5,12 +5,18 @@ const routes = express.Router()
 const PlayerController = require('./app/controllers/PlayerController')
 const CompetitionController = require('./app/controllers/CompetitionController')
 
+// CRUD de Competição
 routes.get('/competitions', CompetitionController.index)
 routes.get('/competitions/:id', CompetitionController.show)
 routes.post('/competitions', CompetitionController.store)
 routes.put('/competitions/:id', CompetitionController.update)
 routes.delete('/competitions/:id', CompetitionController.destroy)
 
-routes.post('/player', PlayerController.store)
+// CRUD de Players
+routes.get('/players', PlayerController.index)
+routes.get('/players/:name', PlayerController.show)
+routes.post('/players', PlayerController.store)
+routes.put('/players/:id', PlayerController.update)
+routes.delete('/players/:id', PlayerController.destroy)
 
 module.exports = routes
